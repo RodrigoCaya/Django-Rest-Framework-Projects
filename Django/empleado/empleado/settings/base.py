7799d368e1140428981d272a17e3f5d1bcba020a
Rodrigo Cayazaya Marín <rodrigo@cayazaya.cl>
2022-08-04 09:26:53 -0400

    Update base.py

--- Django/empleado/empleado/settings/base.py
+++ Django/empleado/empleado/settings/base.py
@@ -3,9 +3,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5$9_^k+ut8zr78^z0hvl_u^8y&f-6yy#b_rh!$v$ln#ho#480c'
-
 # Application definition
 
 INSTALLED_APPS = [
